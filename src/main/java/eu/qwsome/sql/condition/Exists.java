f8d8b00a8cd4218142b537b61c42a7f453e1a6fc
Lukáš Kvídera <kvideral@qwsome.eu>
2022-08-18 18:34:05 +0200

    #30 Implemented exists

---
+++ src/main/java/eu/qwsome/sql/condition/Exists.java
@@ -0,0 +1,62 @@
+package eu.qwsome.sql.condition;
+
+
+import eu.qwsome.sql.Select;
+
+import java.util.Arrays;
+
+import static eu.qwsome.sql.SubselectValueHolder.subselect;
+
+public class Exists implements Condition {
+
+  private final ValueHolder[] values;
+
+
+  Exists(final ValueHolder... values) {
+    this.values = values;
+  }
+
+
+  public static Exists exists(final ValueHolder values) {
+    return new Exists(values);
+  }
+
+
+  public static Exists exists(final Select.ConditionsBuiltPhase subselect) {
+    return new Exists(subselect(subselect));
+  }
+
+
+  @Override
+  public CharSequence get() {
+    final var builder = new StringBuilder();
+    appendTo(builder);
+
+    return builder;
+  }
+
+
+  @Override
+  public ValueConstructor getValues() {
+    final var valueConstructor = new ValueConstructor();
+    Arrays.stream(this.values).forEach(valueConstructor::add);
+    return valueConstructor;
+  }
+
+
+  @Override
+  public void appendTo(final StringBuilder builder) {
+    builder
+      .append("EXISTS ")
+      .append("( ");
+
+    int i = 0;
+    for (; i < this.values.length - 1; i++) {
+      builder.append(this.values[i].getSql())
+        .append(", ");
+    }
+
+    builder.append(this.values[i].getSql())
+      .append(" )");
+  }
+}
---
+++ src/test/java/eu/qwsome/sql/ExistsTest.java
@@ -0,0 +1,68 @@
+package eu.qwsome.sql;
+
+import static eu.qwsome.sql.Column.column;
+import static eu.qwsome.sql.Select.select;
+import static eu.qwsome.sql.ValueLiteral.value;
+import static eu.qwsome.sql.condition.Exists.exists;
+import static eu.qwsome.sql.condition.FieldComparator.comparedField;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ExistsTest {
+
+  @Test
+  void toSql() {
+    final var select = select()
+        .from("table t2")
+        .where(exists(select("t1.abc")
+            .from("table t1")
+            .where(comparedField(column("t1", "z")).isEqualTo(value("k")))));
+
+    final var sql = select.toSql();
+    assertThat(sql).isEqualTo("SELECT * FROM table t2 WHERE EXISTS ( SELECT t1.abc FROM table t1 WHERE t1.z = ? )");
+
+    final var values = select.toValues();
+    assertThat(values).containsExactly("k");
+  }
+
+
+  @Test
+  void complicatedSQL() {
+    final var select = select()
+        .from("table t2")
+        .where(
+            exists(
+                select("t1.abc")
+                    .from("table t1")
+                    .where(comparedField(column("t1", "z")).isEqualTo(value("k"))
+                        .and(comparedField(column("t1", "another")).isLessOrEqualThan(value("anotherVal"))))
+            )
+                .or(comparedField(column("t2", "brm")).isNull())
+                .and(
+                    exists(
+                        select("t3.aby").from("last t3")
+                            .where(
+                                exists(
+                                    select("t4.xaw").from("first t4").where(comparedField(column("t4", "ul")).isNull()))
+                            )
+                    ))
+        );
+
+    final var sql = select.toSql();
+    assertThat(sql).isEqualTo("SELECT *"
+        + " FROM table t2"
+        + " WHERE ( ( EXISTS ("
+        + " SELECT t1.abc FROM table t1 WHERE ( t1.z = ? AND t1.another <= ? "
+        + ") )"
+        + " OR t2.brm IS NULL )"
+        + " AND EXISTS ("
+        + " SELECT t3.aby FROM last t3 WHERE EXISTS ("
+        + " SELECT t4.xaw FROM first t4 WHERE t4.ul IS NULL"
+        + " ) ) )"
+    );
+
+    final var values = select.toValues();
+    assertThat(values).containsExactly("k", "anotherVal");
+  }
+}
